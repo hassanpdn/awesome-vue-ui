bcce8946f59f1ec0a77d751e99f5143b1015443a
hassan <hassanpudineh67@gmail.com>
2023-02-18 01:25:27 +0330

    💡  change tests folder structure

---
+++ src/components/Button/VButton.spec.js
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import Button from '../Button/VButton.vue';
+import { describe, it, expect } from "vitest";
+
+describe('Button', () => {
+      const wrapper = mount(Button)
+      it('renders a vue instance', () => {
+            expect(wrapper.html()).toMatchSnapshot()
+      })
+
+      it('renders a button', () => {
+            expect(wrapper.find('button').exists()).toBe(true)
+      })
+
+      it('should call click handler', async () => {
+            await wrapper.find('button').trigger('click')
+            expect(wrapper.emitted().click).toBeTruthy()
+      })
+
+      it('checks the prop to be passed', () => {
+            const wrapper = mount(Button, {
+                  propsData: {
+                        expanded: true
+                  }
+            })
+            expect(wrapper.classes()).toContain('w-full');
+      })
+
+})
