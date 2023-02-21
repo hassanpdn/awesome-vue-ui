70dfe231c8ef39b23cff2ffc29a6934f52347d88
hassan <hassanpudineh67@gmail.com>
2023-02-21 10:05:32 +0330

    feat: add alert warning style

--- src/components/Alert/VAlert.vue
+++ src/components/Alert/VAlert.vue
@@ -1,11 +1,10 @@
 <template>
-      <div class="alert" :class="[variant_handler , size_handler  ,rounded ? 'alert-rounded' : '',center ? 'alert-center' : '']" v-if="value" ref="alert">
-            <div class="close-alert" v-if="close_able" @click.prevent="$emit('input', !value)">
-                  <svg viewBox="0 0 329.26933 329" xmlns="http://www.w3.org/2000/svg" class="alert-close-icon">
-                        <path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"/>
-                  </svg>
-            </div>
-            {{message}}
+      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Holy smokes!</strong>
+            <span class="block sm:inline">Something seriously bad happened.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
       </div>
 </template>
 
