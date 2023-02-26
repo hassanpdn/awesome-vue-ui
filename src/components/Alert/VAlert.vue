943602e1265ae582a1dff1af8445e68b7ba850e5
hassan <hassanpudineh67@gmail.com>
2023-02-26 13:39:44 +0330

    fix: update overlay style

--- src/components/Alert/VAlert.vue
+++ src/components/Alert/VAlert.vue
@@ -1,10 +1,10 @@
 <template>
-      <!-- <div class="overlay h-screen w-full blur-sm z-10 relative">
-      </div> -->
+      <div class="overlay h-screen w-full blur-sm z-10 relative">
+      </div>
       <div class="alert absolute flex flex-col" :class="[alertClass, sizeClass, {'text-center': center}]" role="alert">
             <strong class="font-bold mt-2">{{ title }}</strong>
             <span class="block sm:inline mb-2">{{ message }}</span>
-            <span v-if="close_able" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <span v-if="close_able" class="absolute flex items-center top-0 bottom-0 right-0 px-4 py-3">
                   <svg class="fill-current h-6 w-6" role="button" @click="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <title>Close</title>
                         <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
